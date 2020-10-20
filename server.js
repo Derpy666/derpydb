79f8d94557272ebb5fedd36406bd53856a84fef6
Derpy <37512753+Derpy666@users.noreply.github.com>
2020-10-20 20:53:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-test 
+const Discord = require("discord.js")
+const bot = new Discord.Client()
+
+let config = require("./config")
+bot.config = config;
+bit.prefix = config.prefix
+
+bot.login(config.token)
