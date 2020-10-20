49016dc8308c26e5548255fe6629261e67a4dd3b
Derpy <37512753+Derpy666@users.noreply.github.com>
2020-10-20 22:30:58 +0300

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,9 +1,41 @@
-module.exports = (bot,db) => {
-
+module.exports = (bot, db) => {
 let methods = {
 all: require("./all")
 }
 
 return {version:"0.0.1"}
 
+function arbitrate(method, params) {
+  let options = {
+    table: params.ops.table || "main"
+  };
+
+  db.prepare(`CREATE TABLE IF NOT EXISTS ${options.table} (id TEXT, value TEXT)`).run();
+
+  if (params.ops.target && params.ops.target[0] === ".")
+    params.ops.target = params.ops.target.slice(1);
+  if (params.data && params.data === Infinity)
+    throw new TypeError(
+      `You cannot set Infinity into the database @ ID: ${params.id}`
+    );
+
+  if (params.stringify) {
+    try {
+      params.data = JSON.stringify(params.data);
+    } catch (e) {
+      throw new TypeError(
+        `Please supply a valid input @ ID: ${params.id}\nError: ${e.message}`
+      );
+    }
+  }
+
+  if (params.id && params.id.includes(".")) {
+    let unparsed = params.id.split(".");
+    params.id = unparsed.shift();
+    params.ops.target = unparsed.join(".");
+  }
+
+  return methods[method](db, params, options);
 }
+}
+
