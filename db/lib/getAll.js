ae218ab7578dc1f31c8121a57ca955f1f72c94ea
Derpy <37512753+Derpy666@users.noreply.github.com>
2020-10-21 20:50:21 +0300

    Update getAll.js

--- db/lib/getAll.js
+++ db/lib/getAll.js
@@ -1,14 +1,7 @@
 module.exports = function(db, params, options) {
   let entry = db
-    .prepare(`SELECT * FROM ${options.table} WHERE ID IS NOT NULL`)
+    .prepare(`SELECT * FROM ${options.table}`)
     .all();
-
-  for (var i = 0; i < entry.length; i++) {
-    entry[i].value =
-      typeof JSON.parse(entry[i].value) == "string"
-        ? JSON.parse(JSON.parse(entry[i].value))
-        : JSON.parse(entry[i].value);
-  }
 
   return entry;
 };
