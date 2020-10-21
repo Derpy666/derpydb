27a2773fe5daa13a28a588eff9ee6d3eb59fb450
Derpy <37512753+Derpy666@users.noreply.github.com>
2020-10-21 22:11:54 +0300

    Update push.js

--- db/lib/push.js
+++ db/lib/push.js
@@ -7,7 +7,7 @@ module.exports = function(db, params, options) {
     .get(params.id);
 
   if (!entry) {
-    db.prepare(`INSERT INTO ${options.table} (ID,value) VALUES (?,?)`).run(
+    db.prepare(`INSERT INTO ${options.table} (ID,${params.ops.target}) VALUES (?,?)`).run(
       params.id,
       "{}"
     );
@@ -17,7 +17,8 @@ module.exports = function(db, params, options) {
   }
 
   if (params.ops.target) {
-    entry = JSON.parse(entry.value);
+    delete entry.id
+    entry = JSON.parse(entry);
     try {
       entry = JSON.parse(entry);
     } catch (e) {}
@@ -31,28 +32,29 @@ module.exports = function(db, params, options) {
     oldArray.push(params.data);
     params.data = set(entry, params.ops.target, oldArray);
   } else {
-    if (entry.value === "{}") entry.value = [];
-    else entry.value = JSON.parse(entry.value);
+    if (entry === "{}") entry = [];
+    else entry = JSON.parse(entry);
     try {
-      entry.value = JSON.parse(entry.value);
+      entry = JSON.parse(entry);
     } catch (e) {}
     params.data = JSON.parse(params.data);
-    if (!Array.isArray(entry.value))
+    if (!Array.isArray(entry))
       throw new TypeError("Target is not an array.");
-    entry.value.push(params.data);
-    params.data = entry.value;
+    entry.push(params.data);
+    params.data = entry;
   }
 
   params.data = JSON.stringify(params.data);
 
-  db.prepare(`UPDATE ${options.table} SET value = (?) WHERE ID = (?)`).run(
+  db.prepare(`UPDATE ${options.table} SET ${params.ops.target} = (?) WHERE ID = (?)`).run(
     params.data,
     params.id
   );
 
   entry = db
     .prepare(`SELECT * FROM ${options.table} WHERE ID = (?)`)
-    .get(params.id).value;
+    .get(params.id);
+delete entry.id
   if (entry === "{}") return null;
   else {
     entry = JSON.parse(entry);
