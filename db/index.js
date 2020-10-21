ad478abc972de0ad6d4782d2d61a603e6913025a
Derpy <37512753+Derpy666@users.noreply.github.com>
2020-10-21 06:50:36 +0300

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,16 +1,32 @@
 module.exports = (bot, db) => {
-let methods = {
-all: require("./lib/all"),
-set: require("./lib/set")
-}
+var methods = {
+  get: require("./lib/get.js"),
+  set: require("./lib/set.js"),
+  has: require("./lib/has.js"),
+  type: require("./lib/type.js"),
+  startsWith: require("./lib/startsWith.js"),
+  push: require("./lib/push.js"),
+  add: require("./lib/add.js"),
+  remove: require("./lib/remove.js"),
+  delete: require("./lib/delete.js"),
+  getAll: require("./lib/getAll.js"),
+  deleteAll: require("./lib/deleteAll.js"),
+  tables: require("./lib/tables.js"),
+  deleteTable: require("./lib/deleteTable.js")
+};
 
 return {
-
-all: function(ops) {
-    return arbitrate("all", { ops: ops || {} });
+  get: function(key, ops) {
+    if (!key) throw new TypeError("No key specified.");
+    return arbitrate("get", { id: key, ops: ops || {} });
   },
 
-set: function(key, value, ops) {
+  fetch: function(key, ops) {
+    if (!key) throw new TypeError("No key specified.");
+    return arbitrate("get", { id: key, ops: ops || {} });
+  },
+
+  set: function(key, value, ops) {
     if (!key) throw new TypeError("No key specified.");
     if (!value && value != 0) throw new TypeError("No value specified.");
     return arbitrate("set", {
@@ -21,8 +37,75 @@ set: function(key, value, ops) {
     });
   },
 
-version:"0.0.1"
-}
+  has: function(key, ops) {
+    if (!key) throw new TypeError("No key specified.");
+    return arbitrate("has", { id: key, ops: ops || {} });
+  },
+
+  type: function(key, ops) {
+    if (!key) throw new TypeError("No key specified.");
+    return arbitrate("type", { id: key, ops: ops || {} });
+  },
+
+  startsWith: function(key, ops) {
+    if (!key) throw new TypeError("No key specified.");
+    return arbitrate("startsWith", { id: key, ops: ops || {} });
+  },
+
+  push: function(key, value, ops) {
+    if (!key) throw new TypeError("No key specified.");
+    if (!value && value != 0)
+      throw new TypeError("Must specify value to push.");
+    return arbitrate("push", {
+      stringify: true,
+      id: key,
+      data: value,
+      ops: ops || {}
+    });
+  },
+
+  add: function(key, value, ops) {
+    if (!key) throw new TypeError("No key specified.");
+    if (isNaN(value)) throw new TypeError("Must specify value to add.");
+    return arbitrate("add", { id: key, data: value, ops: ops || {} });
+  },
+
+  remove: function(key, value, ops) {
+    if (!key) throw new TypeError("No key specified.");
+    if (isNaN(value)) throw new TypeError("Must specify value to remove.");
+    return arbitrate("remove", { id: key, data: value, ops: ops || {} });
+  },
+
+  delete: function(key, ops) {
+    if (!key) throw new TypeError("No key specified.");
+    return arbitrate("delete", { id: key, ops: ops || {} });
+  },
+
+  getAll: function(ops) {
+    return arbitrate("getAll", { ops: ops || {} });
+  },
+
+  all: function(ops) {
+    return arbitrate("getAll", { ops: ops || {} });
+  },
+
+  fetchAll: function(ops) {
+    return arbitrate("getAll", { ops: ops || {} });
+  },
+
+  deleteAll: function(ops) {
+    return arbitrate("deleteAll", { ops: ops || {} });
+  },
+
+  tables: function(ops) {
+    return arbitrate("tables", { ops: ops || {} });
+  },
+
+  deleteTable: function(key, ops) {
+    if (!key) throw new TypeError("No key specified.");
+    return arbitrate("deleteTable", { table: key, ops: ops || {} });
+  }
+};
 
 function arbitrate(method, params) {
   let options = {
