ebff6e68042310cdf8ec92acd313d862e7ec74b1
Derpy <37512753+Derpy666@users.noreply.github.com>
2020-10-22 09:24:09 +0300

    Update startsWith.js

--- db/lib/startsWith.js
+++ db/lib/startsWith.js
@@ -1,6 +1,6 @@
 module.exports = function(db, params, options) {
   let entry = db
-    .prepare(`SELECT * FROM ${options.table} WHERE ID`)
+    .prepare(`SELECT * FROM ${options.table}`)
     .all();
 
   return entry.filter(ent => ent.id.startsWith(params.id));
