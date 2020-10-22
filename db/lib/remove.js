e19635f965d352d715f4497da7dfef8a7f779c50
Derpy <37512753+Derpy666@users.noreply.github.com>
2020-10-22 09:31:50 +0300

    Update remove.js

--- db/lib/remove.js
+++ db/lib/remove.js
@@ -7,37 +7,24 @@ module.exports = function(db, params, options) {
     .get(params.id);
 
   if (!entry) {
-    db.prepare(`INSERT INTO ${options.table} (ID,${params.ops.target}) VALUES (?,?)`).run(
-      params.id,
-      "{}"
+    db.prepare(`INSERT INTO ${options.table} (id) VALUES (?)`).run(
+      params.id
     );
     entry = db
       .prepare(`SELECT * FROM ${options.table} WHERE id = (?)`)
       .get(params.id);
   }
 
-  if (params.ops.target) {
-    delete entry.id
-    entry = JSON.parse(entry);
-    try {
-      entry = JSON.parse(entry);
-    } catch (e) {}
-    params.data = JSON.parse(params.data);
+
+if(params.ops.target == undefined) return null
+
     let oldValue = get(entry, params.ops.target);
     if (oldValue === undefined) oldValue = 0;
     else if (isNaN(oldValue)) throw new Error("Target is not a number.");
     params.data = set(entry, params.ops.target, oldValue - params.data);
-  } else {
-    if (entry === "{}") entry = 0;
-    else entry = JSON.parse(entry);
-    try {
-      entry = JSON.parse(entry);
-    } catch (e) {}
-    if (isNaN(entry)) throw new Error("Target is not a number.");
-    params.data = parseInt(entry, 10) - parseInt(params.data, 10);
-  }
-
-  params.data = JSON.stringify(params.data);
+ 
+    if (isNaN(entry[params.ops.target])) throw new Error("Target is not a number.");
+    params.data = parseInt(entry[params.ops.target]), 10) - parseInt(params.data, 10);
 
   db.prepare(`UPDATE ${options.table} SET ${params.ops.target} = (?) WHERE ID = (?)`).run(
     params.data,
@@ -47,13 +34,7 @@ module.exports = function(db, params, options) {
   entry = db
     .prepare(`SELECT * FROM ${options.table} WHERE ID = (?)`)
     .get(params.id);
-delete entry.id
+
   if (entry === "{}") return null;
-  else {
-    entry = JSON.parse(entry);
-    try {
-      entry = JSON.parse(entry);
-    } catch (e) {}
-    return entry;
-  }
+  else return entry[params.ops.target]
 };
