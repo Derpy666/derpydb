0411dadf357f6889b10fafbdbe497f48b955a4c9
Derpy <37512753+Derpy666@users.noreply.github.com>
2020-10-22 22:14:03 +0300

    Update backup.js

--- db/lib/backup.js
+++ db/lib/backup.js
@@ -5,7 +5,7 @@ module.exports = function(db, params, options) {
 
 function getDate(date) {
     if(!date) date = new Date() 
-    return `${String(date.getMonth()).length == 1 ? `0${date.getMonth()}`: date.getMonth()}-${String(date.getDate()).length == 1 ? `0${date.getDate()}` : date.getDate()}`;
+    return `${String(date.getMonth()).length == 1 ? `0${date.getMonth()}`: date.getMonth()}-${String(date.getDate()).length == 1 ? `0${date.getDate()}` : date.getDate()}-${String(date.getHours()).length == 1 ? `0${date.getHours()}` : date.getHours()}-${String(date.getMinutes()).length == 1 ? `0${date.getMinutes()}` : date.getMinutes()}-${String(date.getSeconds()).length == 1 ? `0${date.getSeconds()}` : date.getSeconds()}`;
   }
 
 /*const fs = require('fs');
