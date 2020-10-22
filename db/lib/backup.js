f3c5298a77731e81f6af2cebeb5f2fafbbdbf910
Derpy <37512753+Derpy666@users.noreply.github.com>
2020-10-22 23:18:29 +0300

    Update backup.js

--- db/lib/backup.js
+++ db/lib/backup.js
@@ -8,21 +8,11 @@ function getDate(date) {
     return `${String(date.getMonth()).length == 1 ? `0${date.getMonth()}`: date.getMonth()}-${String(date.getDate()).length == 1 ? `0${date.getDate()}` : date.getDate()}-${String(date.getHours()).length == 1 ? `0${date.getHours()}` : date.getHours()}-${String(date.getMinutes()).length == 1 ? `0${date.getMinutes()}` : date.getMinutes()}-${String(date.getSeconds()).length == 1 ? `0${date.getSeconds()}` : date.getSeconds()}`;
   }
 
-/*const fs = require('fs');
-
-const path = `./${db.name}`
-
-const content = fs.readFileSync(path)
-
-let attch = new Discord.MessageAttachment(content, `${String(db.name).splite(".")[0]}-${getDate(new Date(fixedDate))}.sqlite`)
-
-return attch*/
-
 let path = db.name
 let backup = `${db.name.split(".")[0]}-${getDate(new Date())}.sqlite`
 
 exec(`copy ${path} ${backup} && move ${backup} backups`)
 
-return `New Backup created (/backups/${backup})`
+return `New Backup created (/db/backups/${backup})`
 
 };
