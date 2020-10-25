e7e5e2106a1db8c3525fb07e09173161305c6a42
Derpy <37512753+Derpy666@users.noreply.github.com>
2020-10-25 13:58:45 +0200

    Update index.js

--- db/index.js
+++ db/index.js
@@ -27,15 +27,20 @@ var methods = {
   createTable: require("./lib/createTable.js")
 };
 
+module.exports = {
+Database: function(path) {
+if(!path) return null
+let db = require("better-sqlite3")(path)
+
 let functions = {
-  /*get: function(key, ops) {
+  get: function(key, ops) {
     if (!key) throw new TypeError("No key specified.");
-    return arbitrate("get", { id: key, ops: ops || {} });
+    return arbitrate("get", { id: key, ops: ops || {},db:db });
   },
 
   fetch: function(key, ops) {
     if (!key) throw new TypeError("No key specified.");
-    return arbitrate("get", { id: key, ops: ops || {} });
+    return arbitrate("get", { id: key, ops: ops || {},db:db });
   },
 
   set: function(key, value, ops) {
@@ -45,23 +50,23 @@ let functions = {
       stringify: true,
       id: key,
       data: value,
-      ops: ops || {}
+      ops: ops || {}, db:db
     });
   },
 
   has: function(key, ops) {
     if (!key) throw new TypeError("No key specified.");
-    return arbitrate("has", { id: key, ops: ops || {} });
+    return arbitrate("has", { id: key, ops: ops || {}, db: db });
   },
 
   type: function(key, ops) {
     if (!key) throw new TypeError("No key specified.");
-    return arbitrate("type", { id: key, ops: ops || {} });
+    return arbitrate("type", { id: key, ops: ops || {}, db: db });
   },
 
   startsWith: function(key, ops) {
     if (!key) throw new TypeError("No key specified.");
-    return arbitrate("startsWith", { id: key, ops: ops || {} });
+    return arbitrate("startsWith", { id: key, ops: ops || {}, db:db });
   },
 
   push: function(key, value, ops) {
@@ -72,29 +77,29 @@ let functions = {
       stringify: true,
       id: key,
       data: value,
-      ops: ops || {}
+      ops: ops || {}, db: db
     });
   },
 
   add: function(key, value, ops) {
     if (!key) throw new TypeError("No key specified.");
     if (isNaN(value)) throw new TypeError("Must specify value to add.");
-    return arbitrate("add", { id: key, data: value, ops: ops || {} });
+    return arbitrate("add", { id: key, data: value, ops: ops || {}, db: db });
   },
 
   remove: function(key, value, ops) {
     if (!key) throw new TypeError("No key specified.");
     if (isNaN(value)) throw new TypeError("Must specify value to remove.");
-    return arbitrate("remove", { id: key, data: value, ops: ops || {} });
+    return arbitrate("remove", { id: key, data: value, ops: ops || {}, db: db });
   },
 
   delete: function(key, ops) {
     if (!key) throw new TypeError("No key specified.");
-    return arbitrate("delete", { id: key, ops: ops || {} });
+    return arbitrate("delete", { id: key, ops: ops || {}, db: db });
   },
 
   getAll: function(ops) {
-    return arbitrate("getAll", { ops: ops || {} });
+    return arbitrate("getAll", { ops: ops || {}, db: db });
   },
 
   all: function(ops) {
@@ -102,72 +107,60 @@ let functions = {
   },
 
   fetchAll: function(ops) {
-    return arbitrate("getAll", { ops: ops || {} });
+    return arbitrate("getAll", { ops: ops || {}, db: db });
   },
 
   deleteAll: function(ops) {
-    return arbitrate("deleteAll", { ops: ops || {} });
+    return arbitrate("deleteAll", { ops: ops || {}, db: db });
   },
 
   tables: function(ops) {
-    return arbitrate("tables", { ops: ops || {} });
+    return arbitrate("tables", { ops: ops || {}, db: db });
   },
 
   backup: function(ops) {
-    return arbitrate("backup", { ops: ops || {} });
+    return arbitrate("backup", { ops: ops || {}, db: db });
   },
 
   backups: function(ops) {
-    return arbitrate("backups", { ops: ops || {} });
+    return arbitrate("backups", { ops: ops || {}, db: db });
   },
 
   download: function(date, ops) {
   if(!date) throw new TypeError("No date specified. (DD-MM-HH-MM-SS)");
-    return arbitrate("download", { date: date, ops: ops || {} });
+    return arbitrate("download", { date: date, ops: ops || {}, db: db });
   },
 
   load: function(date, ops) {
   if(!date) throw new TypeError("No date specified. (DD-MM-HH-MM-SS)");
-    return arbitrate("load", { date: date, ops: ops || {} });
+    return arbitrate("load", { date: date, ops: ops || {}, db: db });
   },
 
   top: function(target, num, ops) {
     if (!target) throw new TypeError("No target top specified.");
     if (!num) throw new TypeError("No top number specified.");
-    return arbitrate("top", { target: target, num: num, ops: ops || {} });
+    return arbitrate("top", { target: target, num: num, ops: ops || {} , db: db});
   },
 
   deleteTable: function(ops) {
-    return arbitrate("deleteTable", { ops: ops || {} });
+    return arbitrate("deleteTable", { ops: ops || {} , db: db});
   },
 
   getTable: function(ops) {
-    return arbitrate("getTable", { ops: ops || {} });
+    return arbitrate("getTable", { ops: ops || {} , db: db});
   },
 
   createTable: function(ops) {
-    return arbitrate("createTable", { ops: ops || {} });
-  },
+    return arbitrate("createTable", { ops: ops || {}, db: db});
+  }
+};
 
-  use: function(p, ops) {
-    if(!p) throw new TypeError("No path specified.");
-    return arbitrate("use", { path: p, ops: ops || {} });
-  },*/
-
-  Database: function(path) {
-if(!path) return null
-let db = require("better-sqlite3")(path)
-
-let tables = (ops) => arbitrate("tables", { ops: ops || {}, db: db });
-
-this.tables = tables;
+Object.keys(functions).map(x => this[x] = functions[x])
 
 },
 
   version: require("../package.json").version
-};
-
-module.exports = functions;
+}
 
 function arbitrate(method, params) {
   let options = {
